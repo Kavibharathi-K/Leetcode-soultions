2aec81faaf177e80f0ba11f2ced86026a181a634
Kavibharathi <96473034+Kavibharathi-K@users.noreply.github.com>
2025-01-29 08:41:53 +0530

    Time: 6 ms (33.13%) | Memory: 42.3 MB (22.21%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,30 +1,21 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        Boolean[][] dp = new Boolean[s1.length() + 1][s2.length() + 1];
+        int n = s1.length();
+        int m = s2.length();
+        boolean[][] dp = new boolean[n + 1][m + 1];
 
-        return backtrack(0, 0, 0, s1, s2, s3, dp);
-    }
+        if(n + m != s3.length()) return false;
 
-    boolean backtrack(int point1, int point2, int point3, String s1, String s2, String s3, Boolean[][] dp)
-    {
-        if(point3 == s3.length())
+        dp[n][m] = true;
+
+        for(int p1 = n; p1 >= 0; p1--)
         {
-            return (point1 == s1.length() && point2 == s2.length());
+            for(int p2 = m; p2 >= 0; p2--)
+            {
+                if(p1 < n && s1.charAt(p1) == s3.charAt(p1 + p2) && dp[p1 + 1][p2]) dp[p1][p2] = true;
+                if(p2 < m && s2.charAt(p2) == s3.charAt(p1 + p2) && dp[p1][p2 + 1]) dp[p1][p2] = true;
+            }
         }
-        if(dp[point1][point2] != null) return dp[point1][point2];
-
-        boolean result = false;
-        if(point1 < s1.length() && s1.charAt(point1) == s3.charAt(point3))
-        {
-            result = backtrack(point1 + 1, point2, point3 + 1, s1, s2, s3, dp);
-        }
-
-            if(!result && point2 < s2.length() && s2.charAt(point2) == s3.charAt(point3)) 
-        {
-            result = backtrack(point1, point2 + 1, point3 + 1, s1, s2, s3, dp);
-        }
-
-        dp[point1][point2] = result;
-        return result;
+        return dp[0][0];
     }
 }
