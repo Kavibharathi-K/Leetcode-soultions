100e7ad593cf3cac812c7617c47244dd53758790
Kavibharathi <96473034+Kavibharathi-K@users.noreply.github.com>
2025-04-01 10:17:58 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (90.01%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -7,11 +7,9 @@ class Solution {
         {
             if(nums[pointer1] != nums[pointer2])
             {
-                pointer1++;
-                nums[pointer1] = nums[pointer2];
-                pointer2++;
+                nums[++pointer1] = nums[pointer2];
             }
-            else pointer2++;
+            pointer2++;
         }
         return pointer1 + 1;
     }
