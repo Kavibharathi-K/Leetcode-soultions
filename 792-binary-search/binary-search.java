333ec36445dcb880e4e381f0d1b28ea684a957e3
Kavibharathi <96473034+Kavibharathi-K@users.noreply.github.com>
2025-09-07 10:56:28 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (90.86%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -7,8 +7,7 @@ class Solution {
         {
             int mid = left + (right - left) / 2;
             if(nums[mid] == target) return mid;
-
-            if(nums[mid] < target) left = mid + 1;
+            else if(nums[mid] < target) left = mid + 1;
             else right = mid - 1;
         }
         return -1;
