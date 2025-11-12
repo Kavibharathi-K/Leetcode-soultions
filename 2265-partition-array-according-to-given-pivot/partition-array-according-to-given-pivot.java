e9b7c6ca223c4049e490eb457b1bf032e1258d60
Kavibharathi <96473034+Kavibharathi-K@users.noreply.github.com>
2025-11-12 09:13:38 +0530

    Time: 5 ms (78.90%) | Memory: 160 MB (8.86%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,27 +1,25 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int left = 0;
-        int right = nums.length - 1;
-        int[] result = new int[nums.length];
-        int p1 = 0;
-        int p2 = nums.length - 1;
-
-        while (left < nums.length && right >= 0) {
-            if (nums[left] < pivot) {
-                result[p1++] = nums[left];
+        int n = nums.length, index = 0;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < pivot) {
+                ans[index++] = nums[i];
             }
-            if (nums[right] > pivot) {
-                result[p2--] = nums[right];
-            }
-            left++;
-            right--;
         }
 
-        int remaining = p2 - p1 + 1;
-        while (remaining > 0) {
-            result[p1++] = pivot;
-            remaining--;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == pivot) {
+                ans[index++] = nums[i];
+            }
         }
-        return result;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > pivot) {
+                ans[index++] = nums[i];
+            }
+        }
+
+        return ans;
     }
 }
