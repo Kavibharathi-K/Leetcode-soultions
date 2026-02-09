428b3b2fcc4e16cd9b4ddeecf668057d7216d347
Kavibharathi <96473034+Kavibharathi-K@users.noreply.github.com>
2026-02-09 11:28:44 +0530

    Time: 6 ms (99.56%) | Memory: 48.3 MB (20.16%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,33 +1,30 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int left = 1;
-        int right = 1;
-        int ans = 0;
+        int right = 0;
+        int result = 0;
 
-        for(int i = 0; i < piles.length; i++)
-        {
-            if(piles[i] > right) right = piles[i];
-        }
+        for(int i = 0; i < piles.length; i++) right = Math.max(right, piles[i]);
 
         while(left <= right)
         {
             int mid = left + (right - left) / 2;
             if(canEat(piles, h, mid))
             {
-                ans = mid;
+                result = mid;
                 right = mid - 1;
             }
             else left = mid + 1;
         }
-        return ans;
+        return result;
     }
 
-    public boolean canEat(int[] piles, int h, int k)
+    public boolean canEat(int[] piles, int h, int bananaPerHour)
     {
         long currentHoursNeeded = 0;
         for(int i = 0; i < piles.length; i++)
         {
-            currentHoursNeeded += (piles[i] + k - 1) / k;
+            currentHoursNeeded += (piles[i] + bananaPerHour - 1) / bananaPerHour;
 
         }
 
