b58f86611cd237b1aaa60aa20806cf080a01f029
Kavibharathi <96473034+Kavibharathi-K@users.noreply.github.com>
2026-02-20 10:41:37 +0530

    Time: 2 ms (90.04%) | Memory: 48.1 MB (5.38%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,12 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int next = 0;
         int pointer = 0;
-        
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(nums[i] != 0) nums[pointer++] = nums[i];
-        }
 
-        while(pointer < nums.length) nums[pointer++] = 0;
+        while(pointer < nums.length)
+        {
+            if(nums[pointer] != 0)
+            {
+                nums[next] = nums[pointer];
+                next++;
+            }
+            pointer++;
+        }
+        while(next < nums.length) nums[next++] = 0;
     }
 }
